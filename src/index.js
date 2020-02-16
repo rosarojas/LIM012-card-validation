bd27a14b57efa2af96f3aff2b3fd60578e7ebabd
rosarojas <60890642+rosarojas@users.noreply.github.com>
2020-02-16 00:28:54 -0500

    proyecto-tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//Nombre de usuario
+let nombreUsuario = document.getElementById("nombreUsuario");
+//Invocar función mostrarString, que es is.valid
+//Invocar función maskify para ocultar caracteres
 import validator from './validator.js';
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,7 @@
-const validator = {
-  // ...
+let tarjeta = document.getElementById("numeroTarjeta").value;
+const validator = (creditCardNumber) => {
+let isValid = alert("Hola"); 
+
 };
 
 export default validator;
